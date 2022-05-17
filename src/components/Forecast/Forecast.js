71bbc512c17d4207f437539f1b39e86f4c0db7ba
anguscg <angus.cg@hotmail.com>
2022-05-17 13:58:55 +1000

    bored styling

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Conditions from '../Conditions/Conditions';
 import classes from './Forecast.module.css'; 
-const axios = require("axios");
+
 
 
 
@@ -52,38 +52,39 @@ function getForecast(e) {
 }
 
     return (
-        <div>
+        <div className={classes.wrapper}>
             <h2>Find Current Weather Conditions</h2>
             <form onSubmit={getForecast}>
                 <input
-                    type="text"
-                    placeholder="Enter City"
-                    maxLength="50"
-                    className={classes.textInput}
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    />
-                <label className={classes.Radio}>
-                    <input
-                        type="radio"
-                        name="units"
-                        checked={unit === "imperial"}
-                        value="imperial"
-                        onChange={(e) => setUnit(e.target.value)}
+                        type="text"
+                        placeholder="Enter City"
+                        maxLength="50"
+                        className={classes.textInput}
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
                         />
-                    Fahrenheit
-                </label>
-                <label className={classes.Radio}>
-                    <input
-                        type="radio"
-                        name="units"
-                        checked={unit === "metric"}
-                        value="metric"
-                        onChange={(e) => setUnit(e.target.value)}
-                        />
-                    Celcius
-                </label>
-
+                <div className={classes.formsection}>
+                    <label className={classes.Radio}>
+                        <input
+                            type="radio"
+                            name="units"
+                            checked={unit === "imperial"}
+                            value="imperial"
+                            onChange={(e) => setUnit(e.target.value)}
+                            />
+                        Fahrenheit
+                    </label>
+                    <label className={classes.Radio}>
+                        <input
+                            type="radio"
+                            name="units"
+                            checked={unit === "metric"}
+                            value="metric"
+                            onChange={(e) => setUnit(e.target.value)}
+                            />
+                        Celcius
+                    </label>
+                </div>
                 <button className={classes.Button} type="submit">Get Forecast</button>
             </form>
             <Conditions
